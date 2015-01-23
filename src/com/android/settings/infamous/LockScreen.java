e08da39a6a25fc98316d5857a94c1ca11c9f08b1
xxwhiterhinoxx <whiterhino0707@gmail.com>
2015-01-23 14:30:57 -0500

    Revert disable camera/dialer from lockscreen...didnt work with cm lockscreen shortcut... also move some other things into lockscreen like launch cLock remove duplicate hide brightness slider from quick settings

--- src/com/android/settings/infamous/LockScreen.java
+++ src/com/android/settings/infamous/LockScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 crDroid Android
+ * Copyright (C) 2015 Infamous Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,9 @@
 
 package com.android.settings.infamous;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.Dialog;
 import android.app.admin.DevicePolicyManager;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -24,47 +27,39 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.os.UserHandle;
+import android.preference.CheckBoxPreference;
 import android.preference.SwitchPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.widget.Toast;
 
+import com.android.settings.infamous.Helpers;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class LockScreen extends SettingsPreferenceFragment
-        implements OnSharedPreferenceChangeListener {
+public class LockScreen extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    private static final String TAG = "LockScreen";
-
-    private static final String KEY_LOCKSCREEN_CAMERA_WIDGET_HIDE = "camera_widget_hide";
-
-    private PreferenceScreen mLockScreen;
-    private SwitchPreference mCameraWidgetHide;
+    private static final String TAG = "LockScreenSettings";
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.infamous_lockscreen);
 
-        PreferenceScreen prefSet = getPreferenceScreen();
-        PackageManager pm = getPackageManager();
-        Resources res = getResources();
+    }
 
-        mLockScreen = (PreferenceScreen) findPreference("lock_screen");
-
-        // Camera widget hide
-        mCameraWidgetHide = (SwitchPreference) findPreference("camera_widget_hide");
-        boolean mCameraDisabled = false;
-        DevicePolicyManager dpm =
-            (DevicePolicyManager) getActivity().getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (dpm != null) {
-            mCameraDisabled = dpm.getCameraDisabled(null);
-        }
-        if (mCameraDisabled){
-            mLockScreen.removePreference(mCameraWidgetHide);
-        }
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
     }
 
     @Override
@@ -72,17 +67,4 @@ public class LockScreen extends SettingsPreferenceFragment
         super.onResume();
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
-    }
 }
